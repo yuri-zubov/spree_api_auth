3bcce14e0701e18ad892a6f0f895edc9a744e540
Chris Whitman <chris@whitmanconsulting.com>
2016-09-30 15:01:31 -0700

    Product API searches can be filtered by option values - https://trello.com/c/huoPPnAL/49-api-search-filters-by-sizes

--- app/controllers/spree/api/v1/products_controller_decorator.rb
+++ app/controllers/spree/api/v1/products_controller_decorator.rb
@@ -6,15 +6,25 @@ module Spree
         def index
           @products = Spree::Product.all
 
-          # Filter products by price if params exist
+          # Filter products  by  price. Both  parameters
+          #  ('price_floor', 'price_ceiling are required
+          #  for the filter to trigger
           if params.has_key?(:price_floor) and params.has_key?(:price_ceiling)
             @products = @products.price_between(params[:price_floor], params[:price_ceiling])
           end
 
-          # Only get products from taxon (category) IDs in params, if they exists
+          # Only get products from taxon (category) IDs
+          # in params, if they exists
           if params.has_key?(:in_taxons)
-            taxon_ids = params[:taxon_ids].split(',')
+            taxon_ids = params[:in_taxons].split(',')
             @products = @products.in_taxons(taxon_ids)
+          end
+
+          # Filter products by their option types (i.e., 'mens-basic-sizes')
+          #  and  option  values (i.e.,  Small,  Medium,  Large, etc.). Both
+          #  parameters are required for it to work.
+          if params.has_key?(:option_type) and params.has_key?(:option_value)
+            @products = @products.with_option_value(params[:option_type], params[:option_value])
           end
 
           @products = @products.distinct.page(params[:page]).per(params[:per_page])
