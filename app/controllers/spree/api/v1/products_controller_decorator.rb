6bb33d48296f3caa78bd681ac03024c2640c1507
Eaven P <eaven@popstand.com>
2016-12-02 12:42:49 -0800

    check if product collection is empty

--- app/controllers/spree/api/v1/products_controller_decorator.rb
+++ app/controllers/spree/api/v1/products_controller_decorator.rb
@@ -27,47 +27,49 @@ module Spree
             end
           end
 
-          # Filter products by gender
-          if params.has_key?(:gender)
-            if params[:gender] == "male"
-              # 7 is the Male parent taxon
-              @products = @products.in_taxons(7)
-            else
-              # 8 is the Female parent taxon
-              @products = @products.in_taxons(8)
+          if @products.present?
+            # Filter products by gender
+            if params.has_key?(:gender)
+              if params[:gender] == "male"
+                # 7 is the Male parent taxon
+                @products = @products.in_taxons(7)
+              else
+                # 8 is the Female parent taxon
+                @products = @products.in_taxons(8)
+              end
             end
+
+            # Filter products by name or description
+            if params.has_key?(:q)
+              @products = @products.in_name_or_description(params[:q])
+            end
+
+
+            # Filter products  by  price. Both  parameters
+            #  ('price_floor', 'price_ceiling are required
+            #  for the filter to trigger
+            if params.has_key?(:price_floor) and params.has_key?(:price_ceiling)
+              @products = @products.price_between(params[:price_floor], params[:price_ceiling])
+            end
+
+            # Only get products from taxon (category) IDs
+            # in params, if they exists
+            if params.has_key?(:in_taxons)
+              taxon_ids = params[:in_taxons].split(',').map(&:to_i)
+              @products = @products.in_taxons(taxon_ids)
+            end
+
+            # Filter products by their option types (i.e., 'mens-basic-sizes')
+            #  and  option  values (i.e.,  Small,  Medium,  Large, etc.). Both
+            #  parameters are required for it to work.
+            if params.has_key?(:option_type) and params.has_key?(:option_value)
+              @products = @products.with_option_value(params[:option_type], params[:option_value])
+            end
+
+            # Pagination
+            @products = @products.distinct.page(params[:page]).per(params[:per_page])
+            @current_api_user = current_api_user
           end
-
-          # Filter products by name or description
-          if params.has_key?(:q)
-            @products = @products.in_name_or_description(params[:q])
-          end
-
-
-          # Filter products  by  price. Both  parameters
-          #  ('price_floor', 'price_ceiling are required
-          #  for the filter to trigger
-          if params.has_key?(:price_floor) and params.has_key?(:price_ceiling)
-            @products = @products.price_between(params[:price_floor], params[:price_ceiling])
-          end
-
-          # Only get products from taxon (category) IDs
-          # in params, if they exists
-          if params.has_key?(:in_taxons)
-            taxon_ids = params[:in_taxons].split(',').map(&:to_i)
-            @products = @products.in_taxons(taxon_ids)
-          end
-
-          # Filter products by their option types (i.e., 'mens-basic-sizes')
-          #  and  option  values (i.e.,  Small,  Medium,  Large, etc.). Both
-          #  parameters are required for it to work.
-          if params.has_key?(:option_type) and params.has_key?(:option_value)
-            @products = @products.with_option_value(params[:option_type], params[:option_value])
-          end
-
-          # Pagination
-          @products = @products.distinct.page(params[:page]).per(params[:per_page])
-          @current_api_user = current_api_user
 
           # Set cache invalidation
           expires_in 15.minutes, :public => true
